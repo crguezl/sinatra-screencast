94266f57ce64d7cad610cbb46b4fc370d2ef432e
Casiano Rodriguez <casiano.rodriguez.leon@gmail.com>
2012-06-28 12:42:49 +0100

    added variable @title in app.rb and modified views/layout.haml

--- app.rb
+++ app.rb
@@ -2,14 +2,17 @@ require 'sinatra'
 require 'haml'
 
 get '/' do
+  @title = 'home'
   haml :index
 end
 
 get '/about' do
+  @title = 'about'
   haml :about
 end
 
 get '/contact' do
+  @title = 'contact'
   haml :contact
 end
 
